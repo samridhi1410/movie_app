22a6d48a09db4ce0531808387f8aba9654341230
Samridhi Bhalla <samridhibhalla14@gmail.com>
2022-02-28 12:11:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,447 @@
+let data=
+{
+  "dates": {
+    "maximum": "2021-11-03",
+    "minimum": "2021-10-13"
+  },
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/t9nyF3r0WAlJ7Kr6xcRYI4jr9jm.jpg",
+      "genre_ids": [
+        878,
+        28
+      ],
+      "id": 580489,
+      "original_language": "en",
+      "original_title": "Venom: Let There Be Carnage",
+      "overview": "After finding a host body in investigative reporter Eddie Brock, the alien symbiote must face a new enemy, Carnage, the alter ego of serial killer Cletus Kasady.",
+      "popularity": 6432.864,
+      "poster_path": "/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
+      "release_date": "2021-09-30",
+      "title": "Venom: Let There Be Carnage",
+      "video": false,
+      "vote_average": 7.5,
+      "vote_count": 208
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/akwg1s7hV5ljeSYFfkw7hTHjVqk.jpg",
+      "genre_ids": [
+        16,
+        35,
+        12,
+        10751
+      ],
+      "id": 459151,
+      "original_language": "en",
+      "original_title": "The Boss Baby: Family Business",
+      "overview": "The Templeton brothers — Tim and his Boss Baby little bro Ted — have become adults and drifted away from each other. But a new boss baby with a cutting-edge approach and a can-do attitude is about to bring them together again … and inspire a new family business.",
+      "popularity": 876.679,
+      "poster_path": "/uWStkK8bq9ixY3fc7y209ZleCoF.jpg",
+      "release_date": "2021-07-01",
+      "title": "The Boss Baby: Family Business",
+      "video": false,
+      "vote_average": 7.8,
+      "vote_count": 1469
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/qD45xHA35HdJDGOaA1AgDwiWEgO.jpg",
+      "genre_ids": [
+        10749,
+        18
+      ],
+      "id": 744275,
+      "original_language": "en",
+      "original_title": "After We Fell",
+      "overview": "Just as Tessa's life begins to become unglued, nothing is what she thought it would be. Not her friends nor her family. The only person that she should be able to rely on is Hardin, who is furious when he discovers the massive secret that she's been keeping. Before Tessa makes the biggest decision of her life, everything changes because of revelations about her family.",
+      "popularity": 738.154,
+      "poster_path": "/3WfvjNWr5k1Zzww81b3GWc8KQhb.jpg",
+      "release_date": "2021-09-01",
+      "title": "After We Fell",
+      "video": false,
+      "vote_average": 8.1,
+      "vote_count": 314
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kTOheVmqSBDIRGrQLv2SiSc89os.jpg",
+      "genre_ids": [
+        16,
+        35,
+        10751
+      ],
+      "id": 639721,
+      "original_language": "en",
+      "original_title": "The Addams Family 2",
+      "overview": "The Addams get tangled up in more wacky adventures and find themselves involved in hilarious run-ins with all sorts of unsuspecting characters.",
+      "popularity": 769.912,
+      "poster_path": "/xYLBgw7dHyEqmcrSk2Sq3asuSq5.jpg",
+      "release_date": "2021-10-01",
+      "title": "The Addams Family 2",
+      "video": false,
+      "vote_average": 8,
+      "vote_count": 137
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/u5Fp9GBy9W8fqkuGfLBuuoJf57Z.jpg",
+      "genre_ids": [
+        12,
+        28,
+        53
+      ],
+      "id": 370172,
+      "original_language": "en",
+      "original_title": "No Time to Die",
+      "overview": "Bond has left active service and is enjoying a tranquil life in Jamaica. His peace is short-lived when his old friend Felix Leiter from the CIA turns up asking for help. The mission to rescue a kidnapped scientist turns out to be far more treacherous than expected, leading Bond onto the trail of a mysterious villain armed with dangerous new technology.",
+      "popularity": 645.84,
+      "poster_path": "/eqWaeh21e4ZgHjwpULZVHCGIq9X.jpg",
+      "release_date": "2021-09-29",
+      "title": "No Time to Die",
+      "video": false,
+      "vote_average": 7.4,
+      "vote_count": 314
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/3pIqd1hgZ2xqzWEyiYp4blqE9Fi.jpg",
+      "genre_ids": [
+        53,
+        36,
+        18
+      ],
+      "id": 522241,
+      "original_language": "en",
+      "original_title": "The Courier",
+      "overview": "Cold War spy Greville Wynne and his Russian source try to put an end to the Cuban Missile Crisis.",
+      "popularity": 582.709,
+      "poster_path": "/zFIjKtZrzhmc7HecdFXXjsLR2Ig.jpg",
+      "release_date": "2021-03-18",
+      "title": "The Courier",
+      "video": false,
+      "vote_average": 7.1,
+      "vote_count": 405
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/5WPKkfVcMtQ0YOvSovbmxvNq3i1.jpg",
+      "genre_ids": [
+        27,
+        53
+      ],
+      "id": 565028,
+      "original_language": "en",
+      "original_title": "Candyman",
+      "overview": "Anthony and his partner move into a loft in the now gentrified Cabrini-Green, and after a chance encounter with an old-timer exposes Anthony to the true story behind Candyman, he unknowingly opens a door to a complex past that unravels his own sanity and unleashes a terrifying wave of violence.",
+      "popularity": 391.861,
+      "poster_path": "/dqoshZPLNsXlC1qtz5n34raUyrE.jpg",
+      "release_date": "2021-08-25",
+      "title": "Candyman",
+      "video": false,
+      "vote_average": 6.5,
+      "vote_count": 419
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/fRrpOILyXuWaWLmqF7kXeMVwITQ.jpg",
+      "genre_ids": [
+        27,
+        53,
+        12,
+        9648
+      ],
+      "id": 522444,
+      "original_language": "en",
+      "original_title": "Black Water: Abyss",
+      "overview": "An adventure-loving couple convince their friends to explore a remote, uncharted cave system in the forests of Northern Australia. With a tropical storm approaching, they abseil into the mouth of the cave, but when the caves start to flood, tensions rise as oxygen levels fall and the friends find themselves trapped. Unknown to them, the storm has also brought in a pack of dangerous and hungry crocodiles.",
+      "popularity": 321.548,
+      "poster_path": "/95S6PinQIvVe4uJAd82a2iGZ0rA.jpg",
+      "release_date": "2020-07-09",
+      "title": "Black Water: Abyss",
+      "video": false,
+      "vote_average": 5.1,
+      "vote_count": 272
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/aknvFyJUQQoZFtmFnYzKi4vGv4J.jpg",
+      "genre_ids": [
+        28,
+        12,
+        18,
+        878
+      ],
+      "id": 438631,
+      "original_language": "en",
+      "original_title": "Dune",
+      "overview": "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence-a commodity capable of unlocking humanity's greatest potential-only those who can conquer their fear will survive.",
+      "popularity": 305.104,
+      "poster_path": "/s1FhMAr91WL8D5DeHOcuBELtiHJ.jpg",
+      "release_date": "2021-09-15",
+      "title": "Dune",
+      "video": false,
+      "vote_average": 8.1,
+      "vote_count": 1353
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/hrzoy8vvUrxQixOM11pwW9AX7Bu.jpg",
+      "genre_ids": [
+        80,
+        18
+      ],
+      "id": 524369,
+      "original_language": "en",
+      "original_title": "The Many Saints of Newark",
+      "overview": "Young Anthony Soprano is growing up in one of the most tumultuous eras in Newark, N.J., history, becoming a man just as rival gangsters start to rise up and challenge the all-powerful DiMeo crime family. Caught up in the changing times is the uncle he idolizes, Dickie Moltisanti, whose influence over his nephew will help shape the impressionable teenager into the all-powerful mob boss, Tony Soprano.",
+      "popularity": 252.585,
+      "poster_path": "/1UkbPQspPbq1FPbFP4VV1ELCfSN.jpg",
+      "release_date": "2021-09-22",
+      "title": "The Many Saints of Newark",
+      "video": false,
+      "vote_average": 6.7,
+      "vote_count": 72
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/kIG81E0H4CxuYIRgyNyD0z3KSlM.jpg",
+      "genre_ids": [
+        27,
+        53,
+        80
+      ],
+      "id": 610253,
+      "original_language": "en",
+      "original_title": "Halloween Kills",
+      "overview": "After Laurie, Karen and Allyson leave the masked monster Michael Myers caged and burning in Laurie's basement, Laurie is rushed to the hospital with fatal injuries, believing she has finally killed her algorithm.  But when Michael manages to free himself from Laurie's trap, his bloodbath ritual begins again. As she fights her pain and prepares to defend against it, she inspires everyone in Haddonfield to rise up against The Shape.",
+      "popularity": 158.901,
+      "poster_path": "/snHgrt2SAEHdI8LLUzV1HgHqPlr.jpg",
+      "release_date": "2021-10-14",
+      "title": "Halloween Kills",
+      "video": false,
+      "vote_average": 0,
+      "vote_count": 0
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/g6wufgtycJCP508tlC3crSYFCgC.jpg",
+      "genre_ids": [
+        37,
+        18
+      ],
+      "id": 749274,
+      "original_language": "en",
+      "original_title": "Cry Macho",
+      "overview": "Mike Milo, a one-time rodeo star and washed-up horse breeder, takes a job from an ex-boss to bring the man's young son home from Mexico.",
+      "popularity": 157.622,
+      "poster_path": "/wqaaWAlXgLNGdAemU7wNvFZ70hr.jpg",
+      "release_date": "2021-09-16",
+      "title": "Cry Macho",
+      "video": false,
+      "vote_average": 6.8,
+      "vote_count": 155
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/2RHjd10wqv57xYzZkNK8Sl09Ddt.jpg",
+      "genre_ids": [
+        16,
+        28,
+        35,
+        14,
+        12
+      ],
+      "id": 768744,
+      "original_language": "ja",
+      "original_title": "僕のヒーローアカデミア THE MOVIE ワールド ヒーローズ ミッション",
+      "overview": "A mysterious group called Humarize strongly believes in the Quirk Singularity Doomsday theory which states that when quirks get mixed further in with future generations, that power will bring forth the end of humanity. In order to save everyone, the Pro-Heroes around the world ask UA Academy heroes-in-training to assist them and form a world-classic selected hero team. It is up to the heroes to save the world and the future of heroes in what is the most dangerous crisis to take place yet in My Hero Academy.",
+      "popularity": 159.07,
+      "poster_path": "/4NUzcKtYPKkfTwKsLjwNt8nRIXV.jpg",
+      "release_date": "2021-08-06",
+      "title": "My Hero Academia: World Heroes' Mission",
+      "video": false,
+      "vote_average": 6.9,
+      "vote_count": 11
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/5MlvT4DZIdkpb7A9t375HVoiJ1v.jpg",
+      "genre_ids": [
+        28,
+        12,
+        18,
+        53
+      ],
+      "id": 646207,
+      "original_language": "en",
+      "original_title": "The Ice Road",
+      "overview": "After a remote diamond mine collapses in far northern Canada, an ice road driver must lead an impossible rescue mission over a frozen ocean to save the trapped miners.",
+      "popularity": 108.46,
+      "poster_path": "/eOH9yh5r7QD4n94gbp7kULf2ft6.jpg",
+      "release_date": "2021-06-24",
+      "title": "The Ice Road",
+      "video": false,
+      "vote_average": 7.2,
+      "vote_count": 681
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/3G6wET9eLvYn3aoIj8NfQFhpYEB.jpg",
+      "genre_ids": [
+        14,
+        878,
+        28,
+        12
+      ],
+      "id": 524434,
+      "original_language": "en",
+      "original_title": "Eternals",
+      "overview": "The Eternals are a team of ancient aliens who have been living on Earth in secret for thousands of years. When an unexpected tragedy forces them out of the shadows, they are forced to reunite against mankind’s most ancient enemy, the Deviants.",
+      "popularity": 118.241,
+      "poster_path": "/6AdXwFTRTAzggD2QUTt5B7JFGKL.jpg",
+      "release_date": "2021-11-03",
+      "title": "Eternals",
+      "video": false,
+      "vote_average": 0,
+      "vote_count": 0
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/4Tz8V8aRim8cFgKEWprSUjBy8tY.jpg",
+      "genre_ids": [
+        18,
+        10402,
+        10749
+      ],
+      "id": 776503,
+      "original_language": "en",
+      "original_title": "CODA",
+      "overview": "As a CODA (Child of Deaf Adults), Ruby is the only hearing person in her deaf family. When the family's fishing business is threatened, Ruby finds herself torn between pursuing her love of music and her fear of abandoning her parents.",
+      "popularity": 113.052,
+      "poster_path": "/BzVjmm8l23rPsijLiNLUzuQtyd.jpg",
+      "release_date": "2021-07-30",
+      "title": "CODA",
+      "video": false,
+      "vote_average": 8.3,
+      "vote_count": 215
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/tFBVXnqmsmoSFR3rbltTfdGIMgV.jpg",
+      "genre_ids": [
+        27,
+        53
+      ],
+      "id": 774021,
+      "original_language": "en",
+      "original_title": "Demonic",
+      "overview": "A young woman unleashes terrifying demons when supernatural forces at the root of a decades-old rift between mother and daughter are ruthlessly revealed.",
+      "popularity": 145.568,
+      "poster_path": "/tcKtX4XWZq6vhGwhmmB8thdaabV.jpg",
+      "release_date": "2021-07-29",
+      "title": "Demonic",
+      "video": false,
+      "vote_average": 5,
+      "vote_count": 51
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/r7dJqfmspzVlHYWXBiKn7NP8ZBw.jpg",
+      "genre_ids": [
+        18,
+        53,
+        80
+      ],
+      "id": 643532,
+      "original_language": "en",
+      "original_title": "The Card Counter",
+      "overview": "William Tell just wants to play cards. His spartan existence on the casino trail is shattered when he is approached by Cirk, a vulnerable and angry young man seeking help to execute his plan for revenge on a military colonel. Tell sees a chance at redemption through his relationship with Cirk. But keeping Cirk on the straight-and-narrow proves impossible, dragging Tell back into the darkness of his past.",
+      "popularity": 54.982,
+      "poster_path": "/60AQOrByV9vDCqy6nt4eOroRild.jpg",
+      "release_date": "2021-09-03",
+      "title": "The Card Counter",
+      "video": false,
+      "vote_average": 6.6,
+      "vote_count": 130
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/99OiyEOsFecBiS5XkhQAB98rlP6.jpg",
+      "genre_ids": [
+        16,
+        878,
+        10751,
+        35
+      ],
+      "id": 482321,
+      "original_language": "en",
+      "original_title": "Ron's Gone Wrong",
+      "overview": "In a world where walking, talking, digitally connected bots have become children's best friends, an 11-year-old finds that his robot buddy doesn't quite work the same as the others do.",
+      "popularity": 60.732,
+      "poster_path": "/gA9QxSravC2EVEkEKgyEmDrfL0e.jpg",
+      "release_date": "2021-10-15",
+      "title": "Ron's Gone Wrong",
+      "video": false,
+      "vote_average": 0,
+      "vote_count": 0
+    },
+    {
+      "adult": false,
+      "backdrop_path": "/2l8Mcy6MD8MoqYmQIuGcKz63Sra.jpg",
+      "genre_ids": [
+        18
+      ],
+      "id": 660943,
+      "original_language": "es",
+      "original_title": "El olvido que seremos",
+      "overview": "Portrays the life of Héctor Abad Gómez, a prominent doctor and human rights activist in the polarized, violent Medellin of the 70s. A family man worried not only for his own children but those of the underprivileged classes as well, his home was imbued with vitality and creativity, the result of an education based on tolerance and love. Nothing could foretell that a terrible cancer would take the life of one of his beloved daughters. Driven by sadness and rage, Héctor devoted himself to the social and political causes of the time. But Medellin's intolerant society would harass him until he was finally silenced.",
+      "popularity": 48.302,
+      "poster_path": "/AnFd9DkA3UGhqmEVtPN9rIIJoH7.jpg",
+      "release_date": "2020-08-22",
+      "title": "Memories of My Father",
+      "video": false,
+      "vote_average": 8.1,
+      "vote_count": 66
+    }
+  ],
+  "total_pages": 19,
+  "total_results": 370
+
+}
+function movieapp()
+{
+
+    let parent= document.getElementById("mainC");
+    console.log(parent);
+    for(let i=0;i<data.results.length;i++)
+    {
+      let student= data.results[i];
+
+      let div= document.createElement("div");
+       div.class="mainC";
+       div.innerHTML=`
+       <div class="cards">
+          <img src="https://image.tmdb.org/t/p/w500/${student.poster_path}">
+          <div id="info">
+              <div id="name">
+                  <h3> ${student.title}</h3>
+        
+              </div>
+              <div id="ratings">
+                  <h3> ${student.vote_average}</h3>
+        
+              </div>
+           </div>
+       </div> 
+      
+  ` ;
+
+       
+   parent.appendChild(div);
+
+    }
+
+}
